5e662dabd6d0c8f286073c94cc831c0f23e054fa
JoudevCoy <joudev09@gmail.com>
2025-10-13 14:26:54 +0800

    adding new files

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnVerif = document.getElementById('gas');
+btnVerif.onclick = (e) => {
+  e.preventDefault();
+  
+  window.location.href = "/login.html"
+}
+
+(function(){
+  // startup code
+  window.location.href = "/login.html"
+})()
